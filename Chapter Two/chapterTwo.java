b31d47e07e94be90c1bcf99d6f7194586c9bda97
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-03-23 17:52:53 -0400

    Getting Workshop applets to view data structures.

---
+++ Chapter Two/chapterTwo.java
@@ -0,0 +1,10 @@
+/*
+ * The Array is the most commonly used data storage strucutre being built into most programming languages. 
+ * A special kind of Array is an ordered array. It is ordered either ascending or descending. This makes it possible to search fastly for a data item using a binary search.
+ * 
+ * Insertion, searching, and deletion are all fundamental operations in data structures going through this book course I am reading. 
+ */
+
+public class chapterTwo {
+    
+}
